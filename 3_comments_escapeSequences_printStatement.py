29686484bfc75fb106cd9fa530a33c5523a9c8e6
d3v-anaxa <d3v.anaxa@gmail.com>
2022-12-03 22:51:13 +0530

    Dec 03 commit

---
+++ 3_comments_escapeSequences_printStatement.py
@@ -0,0 +1,16 @@
+
+print("New line \nand this is a new program");
+
+# Single line comment
+
+''' MULTI-LINE COMMENT
+this is a comment1
+this is a comment2
+this is a comment3
+this is a comment4
+this is a comment5
+this is a comment6
+'''
+
+print("03", "12", "2022", sep="/", end="...\n")
+# end [default] == "\n"
---
+++ 4_variables_dataTypes.py
@@ -0,0 +1,16 @@
+var_int = 14
+print(var_int, type(var_int))
+var_string = "str"
+print(var_string, type(var_string))
+var_bool = True
+print(var_bool, type(var_bool))
+var_complex = complex(12, -7)
+print(var_complex, type(var_complex))
+var_list = [1, True, "newList"] # list is collection of different datatypes
+print(var_list, type(var_list))
+var_tuple = (1, 4 , 11, True, True) # same as list but is immutable
+print(var_tuple, type(var_tuple))
+var_set = {"mkdir", "touch", "vim", ":q", "vim", ":x"} # no duplicates alloewd
+print(var_set, type(var_set))
+var_dictionary = { "name" : "python", "version" : "3.10.6"}
+print(var_dictionary, type(var_dictionary))
