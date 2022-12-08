b56797a07b174e9bab4aca0488af60e0ce68f32b
d3v-anaxa <d3v.anaxa@gmail.com>
2022-12-09 01:36:41 +0530

    Progress : #100DaysOfPython Day_11

---
+++ 9_string.py
@@ -0,0 +1,11 @@
+name = "Anaxa"
+friend1 = "Person_1"
+friend2 = "Person_2"
+para = '''
+This is
+a
+multi-line
+string
+'''
+print("Hello, " + name)
+print(friend1, """said,\"I wants to eat an apple\"""")
