a0ddedc29eefd3d0f573c8e509757f67d2e41e43
Ayanaksha Upadhyaya <d3v.anaxa@gmail.com>
2022-12-15 19:19:35 +0530

    renamed

--- 14_loops.py
+++
@@ -1,8 +0,0 @@
-count = int(input("Enter height : "))
-
-for i in range(count) :
-    print(count - i, end="")
-    for j in range(2 * count) :
-            print("* " if i + j >= count and j - i <= count else "  ", end="")
-
-    print()
