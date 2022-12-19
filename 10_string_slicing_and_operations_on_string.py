2e007deceb14ae6019b09ad2a0c8072548f4a9df
d3v-anaxa <d3v.anaxa@gmail.com>
2022-12-19 22:37:30 +0530

    minor indentation fix

--- 10_string_slicing_and_operations_on_string.py
+++ 10_string_slicing_and_operations_on_string.py
@@ -1,6 +1,6 @@
- name = "Ayanaksha Upadhyaya"
- print(name[0:10])
- print(name[-9:])
- print("My name has", len(name), "characters")
+name = "Ayanaksha Upadhyaya"
+print(name[0:10])
+print(name[-9:])
+print("My name has", len(name), "characters")
 # name = "Harry"
 # print(name[-4:-2])
