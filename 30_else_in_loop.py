15b635a0ddb3551698173b2569465b15ca2616dc
d3v-anaxa <d3v.anaxa@gmail.com>
2023-01-07 02:27:42 +0530

    day_35

---
+++ 30_else_in_loop.py
@@ -0,0 +1,18 @@
+for i in range(5):
+    print(i, end=" ")
+    if(i == 2):
+        break
+
+else:
+    print("\nEnd of loop")
+
+i = 0
+
+while i < 10:
+    print(i, end=" ")
+    i = i+1
+    if(i == 50):
+        break
+
+else:
+    print("\nEnd of loop")
