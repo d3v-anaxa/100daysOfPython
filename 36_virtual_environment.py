0f70e9edcb767b28eed73f56ed2a91b6d751080e
d3v-anaxa <d3v.anaxa@gmail.com>
2023-01-12 18:03:34 +0530

    day_43

---
+++ 36_virtual_environment.py
@@ -0,0 +1,6 @@
+# create v_env => $ python -m venv <dir_name>
+# activate v_env => $ source <path_to_dir>/bin/activate
+# deactivate v_env => $ deactivate
+
+# create 'requirements.txt' => pip freeze > requirements.txt
+# install all packages specified in 'requirements.txt' => pip instal -r <path_to_requirements.txt>
