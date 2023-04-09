674a7ef5afd45670b3d59db372276b7a3beade39
d3v-anaxa <d3v.anaxa@gmail.com>
2023-04-09 21:55:12 +0530

    day_73

---
+++ 60_magic_or_dunder_methods.py
@@ -0,0 +1,4 @@
+fakelist = [ 1, 2, 3]
+print( "length of list : ", fakelist.__len__()) # get length
+print(type(int(1234).__str__()), "->", int(1234).__str__()) # typecast to string
+print(type(int(1240).__repr__()), "->", int(1240).__repr__()) # typecast to string
